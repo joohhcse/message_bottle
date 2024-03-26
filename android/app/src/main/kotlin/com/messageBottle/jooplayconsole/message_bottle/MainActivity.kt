881a5df37e796545cd14fc6d3cd3f19f32c30685
hhjoo <joohhcse@gmail.com>
2024-03-26 16:46:33 +0900

    init

---
+++ android/app/src/main/kotlin/com/messageBottle/jooplayconsole/message_bottle/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.messageBottle.jooplayconsole.message_bottle
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:message_bottle/screen/home_screen.dart';
+
+void main() {
+  runApp(
+    // const MyApp()
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}

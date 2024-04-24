3d2ee35fbb9aa143624670b157e1fdb281ad4d90
hhjoo <joohhcse@gmail.com>
2024-04-24 18:46:18 +0900

    add supabase and module

--- lib/model/msg_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Message {
-  final String id;
-  final String title;
-  final String content;
-
-  Message({required this.id, required this.title, required this.content});
-}
